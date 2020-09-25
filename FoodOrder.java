fb3a32c49d2bd8dc3434f66fff330e253fb58c60
Manoj Kumawat <49870235+gurjarmanoj@users.noreply.github.com>
2020-09-25 10:57:33 +0530

    Add files via upload

---
+++ FoodOrder.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class FoodOrder extends JFrame implements ActionListener{
+	JCheckBox cb1, cb2, cb3;
+	JButton b;
+	public FoodOrder(){
+	
+		JLabel l = new JLabel("Food Ordering System");
+		l.setBounds(50,50,300,20);
+		cb1 = new JCheckBox("Pizza @ 100");
+		cb1.setBounds(100,100,150,20);
+
+		cb2 = new JCheckBox("Burger @ 30");
+		cb2.setBounds(100,150,150,20);
+
+		cb3 = new JCheckBox("Pasta @ 50");
+		cb3.setBounds(100, 200, 150, 20);
+
+		b = new JButton("Order Now");
+		b.setBounds(100, 250, 150, 20);
+
+		b.addActionListener(this);
+
+		add(l); add(cb1); add(cb2); add(cb3); add(b);
+		setSize(800, 800);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e){
+		float amount = 0;
+		String msg = "";
+		if(cb1.isSelected()){
+			amount = amount + 100;
+			msg += "Pizza: 100 Rs \n";
+		}
+		if(cb2.isSelected()){
+			amount = amount + 30;
+			msg += "Burger: 30 Rs \n";
+		}
+		if(cb3.isSelected()){
+			amount = amount + 50;
+			msg += "Pasta: 50 Rs \n";
+		}
+
+		msg += "--------------\n";
+		JOptionPane.showMessageDialog(this,msg+"Total: "+amount);
+	}
+	public static void main(String[] args) {
+		new FoodOrder();
+	}
+}
